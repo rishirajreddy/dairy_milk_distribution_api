e81d056cfb9dfd383086f0912e1379b3bfaae942
rishirajreddy <rishirajreddy.07@gmail.com>
2022-08-13 15:07:04 +0530

    Added another route

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ connectDB();
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json({extended: false}));
 
+app.get("/", (req,res) => {
+    res.send({routes: {
+        "/add": "adding order",
+        "/update/:id": "updating orders",
+        "/updateStatus/:id":"updating order status",
+        "/delete/:id":"deleting orders",
+        "/checkCapacity/:data": "checking milk capacity for the day"
+    }})
+})
+
 app.use("/", orderRoutes);
 
 module.exports = app;
